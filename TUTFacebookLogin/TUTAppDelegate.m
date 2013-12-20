94391697eaf2b9e1482548cdec3d4c0f4a61c15f
Brad Woodard <bcwoodard@gmail.com>
2013-12-20 13:48:48 -0600

    Tweaks to comments.

--- TUTFacebookLogin/TUTAppDelegate.m
+++ TUTFacebookLogin/TUTAppDelegate.m
@@ -77,7 +77,7 @@
 #pragma mark - 3
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Called when the application is about to terminate. Save data if appropriate.
     [self.fbSession close];
 }
 
